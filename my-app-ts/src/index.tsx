2d334abc427845039d22b554a1c5ac475fe1c976
paul <duddnrdpqj@gmail.com>
2023-10-01 16:33:11 +0900

    Learn React Writing Markup with JSX

--- my-app-ts/src/index.tsx
+++ my-app-ts/src/index.tsx
@@ -4,14 +4,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import App from "./App";
+import TodoList from "./App";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <TodoList />
   </React.StrictMode>
 );
 
