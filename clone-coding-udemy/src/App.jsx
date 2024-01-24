374a4010ca6c63e2c0b2175a2e572c6938c3c441
paul <duddnrdpqj@gmail.com>
2024-01-24 23:35:35 +0900

    udemy lecture - children prop

--- clone-coding-udemy/src/App.jsx
+++ clone-coding-udemy/src/App.jsx
@@ -1,6 +1,7 @@
 import {CORE_CONCEPTS} from './data';
 import Header from './components/Header/Header';
 import CoreConcept from './components/CoreConcept';
+import TabButton from './components/TabButton';
 
 function App() {
   return (
@@ -16,6 +17,16 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[3]}/>
           </ul>
         </section>
+        <section id='examples'>
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
+        </section>
+
         <h2>Time to get started!</h2>
       </main>
     </div>
