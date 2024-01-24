39f25584ff92ea3f8115e281cea831b534e81f76
paul <duddnrdpqj@gmail.com>
2024-01-24 22:46:17 +0900

    udemy lecture - Configuring Components With "Props"

--- clone-coding-udemy/src/App.jsx
+++ clone-coding-udemy/src/App.jsx
@@ -1,4 +1,6 @@
-import reactImg from './assets/react-core-concepts.png'
+import reactImg from './assets/react-core-concepts.png';
+import componentsImg from './assets/components.png';
+
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
 function genRandomInt(max) {
@@ -9,7 +11,7 @@ function Header() {
   const description = reactDescriptions[genRandomInt(2)];
   return (
     <header>
-      <img src={reactImg} alt="Stylized atom" />
+      <img src={reactImg} alt='Stylized atom' />
       <h1>React Essentials</h1>
       <p>
         {description} React concepts you will need for almost any app you are going to build!
@@ -18,11 +20,30 @@ function Header() {
   );
 }
 
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
       <Header />
       <main>
+        <section id='core-concepts'>
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept title='Components' description='The Core UI building block' image={componentsImg} />
+            <CoreConcept />
+            <CoreConcept />
+            <CoreConcept />
+          </ul>
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
