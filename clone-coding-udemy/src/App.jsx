e96724594c0d4a01593dd314551ae68d2d03e02b
paul <duddnrdpqj@gmail.com>
2024-01-25 12:54:38 +0900

    udemy lecture - State, Hook 가져오기

--- clone-coding-udemy/src/App.jsx
+++ clone-coding-udemy/src/App.jsx
@@ -1,16 +1,17 @@
-import { CORE_CONCEPTS } from './data';
+import { CORE_CONCEPTS, EXAMPLES } from './data';
 import Header from './components/Header/Header';
 import CoreConcept from './components/CoreConcept';
 import TabButton from './components/TabButton';
 import { useState } from 'react';
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState('Please click a button');
+  const [selectedTopic, setSelectedTopic] = useState('components');
 
-  console.log('start')
+  console.log('start');
+
   function handleClick(selectedButton) {
     setSelectedTopic(selectedButton);
-    console.log(selectedButton)
+    console.log(selectedButton);
   }
 
   return (
@@ -31,11 +32,19 @@ function App() {
           <h2>Examples</h2>
           <menu>
             <TabButton onSelect={() => handleClick('components')}>Components</TabButton>
-            <TabButton onSelect={() => handleClick('JSX')}>JSX</TabButton>
-            <TabButton onSelect={() => handleClick('Props')}>Props</TabButton>
-            <TabButton onSelect={() => handleClick('State')}>State</TabButton>
+            <TabButton onSelect={() => handleClick('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleClick('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleClick('state')}>State</TabButton>
           </menu>
-          {selectedTopic}
+          <div id='tab-content'>
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>
+                {EXAMPLES[selectedTopic].code}
+              </code>
+            </pre>
+          </div>
         </section>
 
         <h2>Time to get started!</h2>
