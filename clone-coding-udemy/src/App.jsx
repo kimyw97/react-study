7883274239e76e1b8fcfc3e9232a6946384554a2
paul <duddnrdpqj@gmail.com>
2024-01-25 12:45:35 +0900

    udemy lecture - State, Hooks

--- clone-coding-udemy/src/App.jsx
+++ clone-coding-udemy/src/App.jsx
@@ -1,12 +1,18 @@
-import {CORE_CONCEPTS} from './data';
+import { CORE_CONCEPTS } from './data';
 import Header from './components/Header/Header';
 import CoreConcept from './components/CoreConcept';
 import TabButton from './components/TabButton';
+import { useState } from 'react';
 
 function App() {
-  function handleClick() {
-    
+  const [selectedTopic, setSelectedTopic] = useState('Please click a button');
+
+  console.log('start')
+  function handleClick(selectedButton) {
+    setSelectedTopic(selectedButton);
+    console.log(selectedButton)
   }
+
   return (
     <div>
       <Header />
@@ -14,21 +20,22 @@ function App() {
         <section id='core-concepts'>
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description} image={CORE_CONCEPTS[0].image} />
-            <CoreConcept {...CORE_CONCEPTS[1]}/>
-            <CoreConcept {...CORE_CONCEPTS[2]}/>
-            <CoreConcept {...CORE_CONCEPTS[3]}/>
+            <CoreConcept title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description}
+                         image={CORE_CONCEPTS[0].image} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
         </section>
         <section id='examples'>
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={handleClick}>Components</TabButton>
-            <TabButton>JSX</TabButton>
-            <TabButton>Props</TabButton>
-            <TabButton>State</TabButton>
+            <TabButton onSelect={() => handleClick('components')}>Components</TabButton>
+            <TabButton onSelect={() => handleClick('JSX')}>JSX</TabButton>
+            <TabButton onSelect={() => handleClick('Props')}>Props</TabButton>
+            <TabButton onSelect={() => handleClick('State')}>State</TabButton>
           </menu>
-          Dynamic Content
+          {selectedTopic}
         </section>
 
         <h2>Time to get started!</h2>
--- clone-coding-udemy/src/components/TabButton.jsx
+++ clone-coding-udemy/src/components/TabButton.jsx
@@ -1,7 +1,7 @@
 export default function TabButton(props) {
   return (
     <li>
-      <button onClick={props.onselect}>{props.children}</button>
+      <button onClick={props.onSelect}>{props.children}</button>
     </li>
   );
 }
