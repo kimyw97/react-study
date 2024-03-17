31d9edb8f595cb6060c5463f53297869ba698110
paul <duddnrdpqj@gmail.com>
2024-03-17 17:35:53 +0900

    temp stop

--- react-native/WeatherClone/App.js
+++ react-native/WeatherClone/App.js
@@ -1,11 +1,60 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
+import { Dimensions, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
 
 export default function App() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+  console.log(text);
+
+
   return (
     <View style={styles.container}>
-      <Text>test</Text>
-      <StatusBar style="auto" />
+      <View style={styles.city}>
+        <Text style={styles.cityName}>{text}</Text>
+      </View>
+      <ScrollView horizontal contentContainerStyle={styles.weather} pagingEnabled={true}
+                  indicatorStyle={'black'}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Clouds</Text>
+        </View>
+
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Clouds</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Clouds</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Clouds</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
@@ -13,8 +62,32 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#049589'
   },
+  city: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  cityName: {
+    fontSize: 68,
+    fontWeight: '500'
+
+  },
+  weather: {
+    backgroundColor: '#fff'
+  },
+  day: {
+    width: Dimensions.get('window').width,
+    flex: 1,
+    alignItems: 'center'
+  },
+  temp: {
+    marginTop: 38,
+    fontSize: 158
+  },
+  description: {
+    marginTop: -30,
+    fontSize: 60
+  }
 });
